b03a7df19968f1c2dae42e5405322b387994c962
Swaroop <swaroopsamantray13@gmail.com>
2026-06-16 22:51:22 +0530

    frontend trigger testing 4

--- product_backend/src/main/java/com/example/product_backend/Application.java
+++ product_backend/src/main/java/com/example/product_backend/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
 	public static void main(String[] args) {
 		var context = SpringApplication.run(Application.class, args);
 
